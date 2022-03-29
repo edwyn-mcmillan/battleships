16242d0cd1805a4ff8ecbdc2356748df217ec1d2
Edwyn McMillan <codemcmillan@gmail.com>
2022-03-29 13:02:53 +1300

    revised solution

--- battleships.js
+++ battleships.js
@@ -1,10 +1,10 @@
 const readline = require("readline");
 
 const MAX_GUESSES = 20;
-var guessCount = 0;
-
+const PROMPT_ = "Guess the X and Y coordinates (two numbers between 0 - 7): ";
+const ships = [];
 const NUM_SHIPS = 2;
-var ships = [];
+let guessCount = 0;
 
 class Ship {
   constructor(x, y) {
@@ -21,37 +21,41 @@ class Ship {
 function gameInit() {
   generateShips();
   console.log("Begin Game!");
+  console.log(ships);
 }
 
 function generateShips() {
-  let long = [0, 1, 2, 3, 4, 5, 6, 7];
-  let lat = [0, 1, 2, 3, 4, 5, 6, 7];
-  shuffleArray(long);
-  shuffleArray(lat);
+  let occupiedCoord = [];
 
-  for (let i = 0; i < NUM_SHIPS; i++) {
-    ships.push(new Ship(long[i], lat[i]));
+  while (ships.length < NUM_SHIPS) {
+    let randomX = getRandomInt(0, 7);
+    let randomY = getRandomInt(0, 7);
+    let newCoord = [randomX, randomY];
+
+    if (!exists(occupiedCoord, newCoord)) {
+      ships.push(new Ship(randomX, randomY));
+      occupiedCoord.push(newCoord);
+    }
   }
 }
 
-function shuffleArray(array) {
-  for (var i = array.length - 1; i > 0; i--) {
-    var j = Math.floor(Math.random() * (i + 1));
-    var temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
-  }
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function exists(arr, search) {
+  return arr.some((row) => row.includes(search));
 }
 
 function checkContact(x, y) {
   for (i = 0; i < ships.length; i++) {
-    if (x == ships[i].x && y == ships[i].y) {
+    if (x == ships[i].x && y == ships[i].y && !ships[i].isSunk) {
       ships[i].hit();
       console.log("Direct Hit!");
       return;
     }
 
-    if (ships[i].isSunk == false) {
+    if (!ships[i].isSunk) {
       let distance = Math.abs(x - ships[i].x) + Math.abs(y - ships[i].y);
       if (distance <= 2) {
         console.log("hot");
@@ -75,28 +79,35 @@ function getUserInput(prompt, callback) {
   });
 }
 
-const prompt_ = "Guess the X and Y coordinates (two numbers between 0 - 7): ";
-
 function play(guess) {
-  let [x, y] = guess.match(/[0-9]/g);
-  guessCount++;
-  console.log("Guesses remaining: ", 20 - guessCount);
-  console.log("Your guess: ", x, y);
-
-  checkContact(x, y);
-
-  if (boardIsEmpty()) {
-    console.log("You win!");
-  } else if (guessCount >= MAX_GUESSES) {
-    console.log("You lose (max number of guesses reached)");
+  if (!guess.match(/[0-9]/g)) {
+    console.log("Invalid Guess");
+    getUserInput(PROMPT_, play);
   } else {
-    getUserInput(prompt_, play);
+    let [x, y] = guess.match(/[0-9]/g);
+    guessCount++;
+    console.log("Guesses remaining: ", 20 - guessCount);
+    console.log("Your guess X:", x, " Y:", y);
+
+    checkContact(x, y);
+
+    if (boardIsEmpty()) {
+      console.log("You win!");
+    } else if (guessCount >= MAX_GUESSES) {
+      console.log("You lose (max number of guesses reached)");
+    } else {
+      getUserInput(PROMPT_, play);
+    }
   }
 }
 
 function boardIsEmpty() {
-  return ships[0].isSunk && ships[1].isSunk;
+  function checkSunk(ship) {
+    return ship.isSunk;
+  }
+
+  return ships.every(checkSunk);
 }
 
 gameInit();
-getUserInput(prompt_, play);
+getUserInput(PROMPT_, play);
